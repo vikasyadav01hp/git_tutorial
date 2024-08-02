83dbe8eb29f10766c19ebc07c865bbe706a70f4d
vikas <vikasyadav01hp@outlook.com>
2024-08-02 10:38:25 +0530

    new file

---
+++ index.js
@@ -0,0 +1,10 @@
+var arr = [1,2,3,4,5,6,7,8,9,10];
+for( var k in arr ){
+	console.log( arr[k] ); 
+	console.log( arr[k] * 2 );
+}
+
+var arrBB = ['A','B','C','D','E'];
+for( var k in arrBB ){
+	console.log( arrBB[k] ); 
+}
